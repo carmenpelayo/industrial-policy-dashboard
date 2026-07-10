6eb525abf810819d5c132aab5f0605a86c604e08
Carmen Pelayo Fernández <90655687+carmenpelayo@users.noreply.github.com>
2026-07-10 10:27:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import openpyxl
 
 # ==========================================
 # 1. BBVA BRANDING & CONFIGURATION
